fcd76b47d479d1fa105fa11023a3a949cc22c995
Douglas Rios - Trabalho <glinhas55@hotmail.com>
2023-03-14 12:07:32 -0300

    continuando

--- checkpoint-front3/src/App.jsx
+++ checkpoint-front3/src/App.jsx
@@ -9,85 +9,103 @@ export default function App() {
     const [descricao, setDescricao] = useState('');
 
     const [listaToDo, setListaToDo] = useState([]);
-    
-    function addTarefa(event) {
-      event.preventDefault();
-      
-      if(titulo === "" || categoria === "" || data === "" || descricao == ""){
-          alert("PREENCHA TODAS AS INFORMAÇÕES")
-          return;
-        }
-        
-        if(id){
-            // const copiaListaToDo = [...listaToDo];
-            // const index = copiaListaToDo.findIndex((tarefa) => tarefa.id === id);
-            // copiaListaToDo[index].titulo = titulo;
-            // copiaListaToDo[index].categoria = categoria;
-            // copiaListaToDo[index].data = data;
-            // copiaListaToDo[index].descricao = descricao;
-            // console.log(copiaListaToDo);
-            const copiaListaToDo = JSON.parse(JSON.stringify(listaToDo))
-            const index = copiaListaToDo.findIndex((tarefa)=>tarefa.id === id)
-            copiaListaToDo[index].titulo = titulo
-            copiaListaToDo[index].categoria = categoria
-            copiaListaToDo[index].data = data
-            copiaListaToDo[index].descricao = descricao
-            setListaToDo(copiaListaToDo)
-            console.log(listaToDo);
-        }else{
-            setListaToDo([...listaToDo, {
-                id: Date.now(), 
-                titulo, 
-                categoria, 
-                data, 
-                descricao
-            }]);
-            console.log(listaToDo);     
-        }
-    
-    setTitulo("");
-    setCategoria("");
-    setData("");
-    setDescricao("");
 
+    function addTarefa(event) {
+        event.preventDefault();
+
+        if (
+            titulo === '' ||
+            categoria === '' ||
+            data === '' ||
+            descricao == ''
+        ) {
+            alert('PREENCHA TODAS AS INFORMAÇÕES');
+            return;
+        }
+
+        {
+            setListaToDo([
+                ...listaToDo,
+                {
+                    id: Date.now(),
+                    titulo,
+                    categoria,
+                    data,
+                    descricao,
+                },
+            ]);
+        }
+
+        setTitulo('');
+        setCategoria('');
+        setData('');
+        setDescricao('');
+        setId('');
+        console.log(id);
     }
 
-//FUNÇÃO EDITAR TAREFA ESTÁ PRONTA MAS NÃO VINCULADA A BOTÕES
-    // function editarTarefa(tarefa){
-    //     setTitulo(tarefa.titulo);
-    //     setCategoria(tarefa.categoria);
-    //     setData(tarefa.data);
-    //     setDescricao(tarefa.descricao)
-    // }
+    function editarTarefas(e) {
+        e.preventDefault();
 
-//FUNÇÃO APAGAR TAREFA ESTÁ PRONTA MAS NÃO VINCULADA A BOTÕES
-    // function apagarTarefa(id){
-    //     if(confirm("Deseja apagar tarefa?")){
-    //         const result = listaToDo.filter((tarefa) => tarefa.id !== id);
-    //         setListaToDo(result);;
-    //     }
-    // }
+        if (id) {
+            const copiaListaToDo = JSON.parse(JSON.stringify(listaToDo));
+            const index = copiaListaToDo.findIndex(tarefa => tarefa.id === id);
+            copiaListaToDo[index].titulo = titulo;
+            copiaListaToDo[index].categoria = categoria;
+            copiaListaToDo[index].data = data;
+            copiaListaToDo[index].descricao = descricao;
+            setListaToDo(copiaListaToDo);
+            console.log(listaToDo);
+        }
+    }
+
+    //FUNÇÃO EDITAR TAREFA ESTÁ PRONTA MAS NÃO VINCULADA A BOTÕES
+    function preencherTarefa(tarefa) {
+        setTitulo(tarefa.titulo);
+        setCategoria(tarefa.categoria);
+        setData(tarefa.data);
+        setDescricao(tarefa.descricao);
+    }
+
+    //FUNÇÃO APAGAR TAREFA ESTÁ PRONTA MAS NÃO VINCULADA A BOTÕES
+    function apagarTarefa(id) {
+        if (confirm('Deseja apagar tarefa?')) {
+            const result = listaToDo.filter(tarefa => tarefa.id !== id);
+            setListaToDo(result);
+        }
+    }
 
     return (
         <div className="bg-black w-full h-screen flex justify-center itens-center p-2">
             <div className="w-1/4 my-20 bg-white mr-5 rounded-lg grid justify-center content-center">
-                <h1 className="mb-5">To Do List</h1>
-                <div className="w-1/4 my-20 bg-white mr-5 rounded-lg">
-                    <form className="grid" onSubmit={addTarefa}>
+                <h1 className="mb-5">Cadastrar Tarefas</h1>
+                <div className="w-1/4 bg-white mr-5 rounded-lg">
+                    <form
+                        className="grid"
+                        onSubmit={id ? editarTarefas : addTarefa}
+                    >
                         <input
                             value={titulo}
                             placeholder="Título"
-                            className="border border-slate-400 rounded-md" 
-                            onChange={(event) => setTitulo (event.target.value)}
+                            className="border border-slate-400 rounded-md"
+                            onChange={event => setTitulo(event.target.value)}
                         />
-                        <div className="text-xs text-red-600 mb-3">
-                            Campo Obrigatório
-                        </div>
+                        {titulo === '' ? (
+                            <div className="text-xs text-red-600 mb-3">
+                                Campo Obrigatório
+                            </div>
+                        ) : (
+                            <div className="mb-3"></div>
+                        )}
 
-                        <select className="mb-3 border border-slate-400 rounded-md" 
-                        value={categoria}
-                        onChange={(event) => setCategoria (event.target.value)}>
-                            <option value="" disabled>Selecione uma categoria</option>
+                        <select
+                            className="mb-3 border border-slate-400 rounded-md"
+                            value={categoria}
+                            onChange={event => setCategoria(event.target.value)}
+                        >
+                            <option value="" disabled>
+                                Selecione uma categoria
+                            </option>
                             <option value="trabaho">Trabaho</option>
                             <option value="lazer">Lazer</option>
                             <option value="prioridade">Prioridade</option>
@@ -98,43 +116,40 @@ export default function App() {
                             className="mb-3 border border-slate-400 rounded-md"
                             type="date"
                             value={data}
-                            placeholder="Data" 
-                            onChange={(event) => setData (event.target.value)}
+                            placeholder="Data"
+                            onChange={event => setData(event.target.value)}
                         />
                         <input
                             className="mb-3 h-20 text-justify border border-slate-400 rounded-md"
                             value={descricao}
-                            placeholder="Descrição" 
-                            onChange={(event) => setDescricao (event.target.value)}
+                            placeholder="Descrição"
+                            onChange={event => setDescricao(event.target.value)}
                         />
 
                         <input
                             className="bg-slate-500 rounded hover:bg-slate-400 hover:text-slate-500"
                             type="submit"
-                            value={id ? "Salvar" : "Cadastrar Tarefa"}
+                            value={id ? 'Salvar' : 'Cadastrar Tarefa'}
                         />
                     </form>
-
-                    {/* <ul>Lista de Tarefas</ul> */}
-                    <h2>Lista de Tarefas</h2>
-                    <ul>
-                        {
-                            listaToDo.map((tarefa)=>(
-                                <Tarefas 
-                                    key={Tarefas.id}
-                                    titulo={tarefa.titulo}
-                                    categoria={tarefa.categoria}
-                                    data={tarefa.data}
-                                    descricao={tarefa.descricao}
-                                />
-                            ))
-                        }
-                    </ul>
-                    
                 </div>
-                <div className="w-1/2 bg-white rounded-lg">itens</div>
+            </div>
+            <div className="w-1/2 bg-white rounded-lg p-2">
+                <h1>Minhas Tarefas</h1>
+                <ul className="w-full bg-orange-500 rounded-md">
+                    {listaToDo.map(tarefa => (
+                        <Tarefas
+                            editar={() => preencherTarefa(tarefa)}
+                            excluir={() => apagarTarefa(tarefa.id)}
+                            key={Tarefas.id}
+                            titulo={tarefa.titulo}
+                            categoria={tarefa.categoria}
+                            data={tarefa.data}
+                            descricao={tarefa.descricao}
+                        />
+                    ))}
+                </ul>
             </div>
         </div>
     );
 }
-
--- checkpoint-front3/src/components/Tarefas/index.jsx
+++ checkpoint-front3/src/components/Tarefas/index.jsx
@@ -1,14 +1,29 @@
-export default function Tarefas(props){
-    const {key, titulo, categoria, data, descricao, editar, excluir} = props;  
-    return(
-        <li key={key} className="flex ">
-            <h1>{titulo}</h1>
-            <p>{categoria}</p>
-            <p>{descricao}</p>
-            <h2>{data}</h2>
-            <img src="../assets/deletar.png" onClick={excluir} alt="imagem de deletar" />
-            <img src="../assets/editar.png" onClick={editar} alt="imagem de editar" />
+export default function Tarefas(props) {
+    const { key, titulo, categoria, data, descricao, editar, excluir } = props;
+    return (
+        <li key={key} className="flex justify-between">
+            <div className="p-2 w-3/4">
+                <h1 className="text-xl font-semibold">{titulo}</h1>
+                <p>{categoria}</p>
+                <p>{descricao}</p>
+            </div>
+            <div className="p-2 grid justify-items-center">
+                <h2 className="text-xl font-semibold">{data}</h2>
+                <div className="flex justify-center items-center">
+                    <img
+                        className="w-6 h-6 mr-4 hover:bg-slate-400 rounded-md"
+                        src="./assets/deletar.png"
+                        onClick={excluir}
+                        alt="imagem de deletar"
+                    />
+                    <img
+                        className="w-5 h-5 hover:bg-slate-400 rounded-sm"
+                        src="./assets/editar.png"
+                        onClick={editar}
+                        alt="imagem de editar"
+                    />
+                </div>
+            </div>
         </li>
-    )
-    
-} 
+    );
+}
